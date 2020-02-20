fce02eb1f7a848f7927027f08caa87bbe6ecc928
Patrick Grady <patickrgrady81@gmail.com>
2020-02-20 13:37:21 -0500

    Established a connection to a sqlite3 database

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,9 @@ ENV['RACK_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/development.sqlite"
+)
+
 require_all 'app'
