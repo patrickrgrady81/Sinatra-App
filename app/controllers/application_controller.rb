0d0ceeb9647bb827bc536a48a9e54050d905fc32
Patrick Grady <patickrgrady81@gmail.com>
2020-02-22 11:38:24 -0500

    Added basic sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,32 @@ class ApplicationController < Sinatra::Base
 
   get "/" do
     @session = session
+    @session[:input_email] ||= ""
   	erb :index
+  end
+
+  get "/users/new" do 
+    @session = session
+    @session[:error] ||= ""
+    erb :new_user
+  end
+
+  post "/users" do 
+    @session = session
+    @session[:input_email] = params[:user_email]
+    # make sure user_password == confirm_password
+    if params[:user_password] != params[:confirm_password]
+      @session[:error] = "Your passwords must match"
+      redirect "/users/new"
+    end
+    # check to see if password is properly formatted using regex
+    # check db if email is taken
+    # maybe check to see password is in a certain format
+    # if everything is ok, add this new user to the db
+    #     set the session to the users email
+    # if not redirect to '/users/new' with an error message
+
+    @session[:error] = "Sorry wrong info"
+    redirect "/users/new"
   end
 end
--- db/migrate/20200222155240_users.rb
+++ db/migrate/20200222155240_users.rb
@@ -1,4 +1,7 @@
 class Users < ActiveRecord::Migration[5.2]
   def change
+    create_table :users do |t|
+      t.string :email
+    end
   end
 end
