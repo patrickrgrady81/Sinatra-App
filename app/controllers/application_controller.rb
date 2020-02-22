569c326f1a2deedbaeaeb67f8a43f51030f83102
Patrick Grady <patickrgrady81@gmail.com>
2020-02-22 12:01:37 -0500

    Added basic sign in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,33 +8,55 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
+    logout
     @session = session
-    @session[:input_email] ||= ""
   	erb :index
+  end
+
+  get "/users/login" do 
+    @session = session 
+    @session[:type] = "login"
+    erb :login
   end
 
   get "/users/new" do 
     @session = session
+    @session[:type] = "signup"
     @session[:error] ||= ""
     erb :new_user
   end
 
   post "/users" do 
     @session = session
-    @session[:input_email] = params[:user_email]
-    # make sure user_password == confirm_password
-    if params[:user_password] != params[:confirm_password]
-      @session[:error] = "Your passwords must match"
-      redirect "/users/new"
-    end
-    # check to see if password is properly formatted using regex
-    # check db if email is taken
-    # maybe check to see password is in a certain format
-    # if everything is ok, add this new user to the db
-    #     set the session to the users email
-    # if not redirect to '/users/new' with an error message
+    if @session[:type] == "signup"
+      @session[:input_email] = params[:user_email]
+      # make sure user_password == confirm_password
+        if params[:user_password] != params[:confirm_password]
+          @session[:error] = "Your passwords must match"
+          redirect "/users/new"
+        end
+      # check to see if password is properly formatted using regex
+      # check db if email is taken
+      # maybe check to see password is in a certain format
+      # if everything is ok, add this new user to the db
+      #     set the session to the users email
+      # if not redirect to '/users/new' with an error message
 
-    @session[:error] = "Sorry wrong info"
-    redirect "/users/new"
+      @session[:error] = "Sorry can't log in right now"
+      redirect "/users/new"
+
+    else  #@session[:type] == "login"
+      # check if email is in db
+      # if no, error = "Invalid username or password"
+      # if yes, check to make sure password is correct
+      # if no, error = "Invalid username or password"
+      # if yes, set session[:email] to user email, user is now logged in
+    end
+  end
+
+  helpers do 
+    def logout
+      session.clear
+    end
   end
 end
